2cc3c5898c3b4e6d802a8ef82075832326db0f46
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-07-19 03:13:26 +0200

    test: read and display watchers

--- public/app.js
+++ public/app.js
@@ -1,14 +1,27 @@
+import _ from 'lodash';
 import moment from 'moment';
 import chrome from 'ui/chrome';
 import uiModules from 'ui/modules';
 import uiRoutes from 'ui/routes';
 import $ from 'jquery';
 
+/* Timepicker */
 import 'ui/timepicker';
 import 'ui/filter_bar';
-// import Notifier from 'ui/notify/notifier';
 
+import TableVisTypeProvider from 'ui/template_vis_type/TemplateVisType';
+// import VisSchemasProvider from 'ui/vis/schemas';
+// import tableVisTemplate from 'plugins/table_vis/table_vis.html';
+
+import AggResponseTabifyTabifyProvider from 'ui/agg_response/tabify/tabify';
+// import tableSpyModeTemplate from 'plugins/spy_modes/table_spy_mode.html';
+
+require('ui/registry/vis_types').register(TableVisTypeProvider);
+
+// import Notifier from 'ui/notify/notifier';
 // import 'ui/autoload/styles';
+
+/* Custom Template + CSS */
 import './less/main.less';
 import template from './templates/index.html';
 
@@ -35,10 +48,10 @@ uiRoutes
         return resp.data.time;
       });
     },
-    currentItems($http) {
-      return $http.get('../api/kaae/getitems').then(function (resp) {
+    currentWatchers($http) {
+      return $http.get('../api/kaae/status').then(function (resp) {
 	console.log('DEBUG RESPONSE:',resp);
-        return resp.data.items;
+        return resp;
       });
     }
   }
@@ -46,13 +59,28 @@ uiRoutes
 
 uiModules
 .get('api/kaae', [])
-.controller('kaaeHelloWorld', function ($scope, $route, $interval, timefilter) {
+.controller('kaaeHelloWorld', function ($scope, $route, $interval, timefilter, Private) {
   $scope.title = 'Kaae';
   $scope.description = 'Kibana Alert App for Elasticsearch';
   $scope.store = window.sessionStorage;
   timefilter.enabled = true;
 
-  $scope.items = $route.current.locals.currentItems;
+  const tabifyAggResponse = Private(AggResponseTabifyTabifyProvider);
+  if ($route.current.locals.currentWatchers.data.hits.hits) {
+	   $scope.watchers = $route.current.locals.currentWatchers.data.hits.hits;
+
+	/*
+	   $scope.spy.params.spyPerPage = 10;
+	   $scope.table = tabifyAggResponse($scope.vis, $route.current.locals.currentWatchers.data.hits.hits, {
+             canSplit: false,
+             asAggConfigResults: true,
+             partialRows: true
+           });
+	*/
+
+  } else { $scope.watchers = []; }
+
+  $scope.items = {};
 
   var currentTime = moment($route.current.locals.currentTime);
   $scope.currentTime = currentTime.format('HH:mm:ss');
