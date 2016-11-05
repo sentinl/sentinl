f27871a232e0557f6fa4c16b86097d0bf4b87da5
Fabio Corneti <info@corneti.com>
2016-11-05 16:05:57 +0000

    Wait for all plugins to be initialised and for ES to be in green status before bootstrap.

--- init.js
+++ init.js
@@ -19,30 +19,42 @@
 
 import later from 'later';
 import _ from 'lodash';
-import mustache from 'mustache';
 import masterRoute from './server/routes/routes';
 import scheduler from './server/lib/scheduler';
 import helpers from './server/lib/helpers';
-import $window from 'jquery';
+import getSentinelClient from './server/lib/get_sentinel_client';
 
-module.exports = function (server, options) {
+const init = _.once((server) => {
+  var config = require('./server/lib/config');
 
-      var debug = false;
-      var config = require('./server/lib/config');
+  server.log(['status', 'info', 'KaaE'], 'KaaE Initializing');
+  server.kaaeStore = [];
 
-      var $ = require('jquery');
-      server.log(['status', 'info', 'KaaE'], 'KaaE Initializing');
-      server.kaaeStore = [];
-      masterRoute(server);
+  masterRoute(server);
 
-      // Create KaaE Indices, if required
-      helpers.createKaaeIndex(server,config);
-      helpers.createKaaeAlarmIndex(server,config);
+  // Create KaaE Indices, if required
+  helpers.createKaaeIndex(server,config);
+  helpers.createKaaeAlarmIndex(server,config);
 
-      /* Bird Watching and Duck Hunting */
-      var client = server.plugins.elasticsearch.client;
-      var sched = later.parse.recur().on(25,55).second();;
-      var t = later.setInterval(function(){ scheduler.doalert(server,client) }, sched);
-      /* run NOW, plus later */
-      scheduler.doalert(server,client);
+  /* Bird Watching and Duck Hunting */
+  const client = getSentinelClient(server);
+  var sched = later.parse.recur().on(25,55).second();
+  var t = later.setInterval(function(){ scheduler.doalert(server,client) }, sched);
+  /* run NOW, plus later */
+  scheduler.doalert(server,client);
+});
+
+export default function (server, options) {
+
+  let status = server.plugins.elasticsearch.status;
+  if (status && status.state === 'green') {
+      init(server);
+  } else {
+    status.on('change', () => {
+      if (server.plugins.elasticsearch.status.state === 'green') {
+        init(server);
+      }
+    });
+  }
+
 };
