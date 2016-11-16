3ce365ee6c96eccbaf701a0fe2f4d2442f17a0b8
Fabio Corneti <info@corneti.com>
2016-11-16 09:32:37 +0000

    Get Horseman from kibi_access_control if available

---
+++ server/lib/horseman_factory.js
@@ -0,0 +1,25 @@
+import Promise from 'bluebird';
+
+/**
+ * Returns a Promise resolved with a Horseman instance.
+ *
+ * @param {Server} server - A Server instance.
+ * @param {string} url - An optional authentication domain.
+ *
+ * @return {Promise}.
+ */
+export default function horsemanFactory(server, domain) {
+
+  let horseman;
+
+  if (server.plugins.kibi_access_control) {
+    return server.plugins.kibi_access_control.getSentinlHorseman(domain);
+  } else {
+    try {
+      const Horseman = require('node-horseman');
+      return Promise.resolve(new Horseman());
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
