414826016b51dfea3616fa1d452b57b3ab790fa1
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2016-12-05 14:36:59 +0100

    Minor fixes for issue #45

--- server/lib/actions.js
+++ server/lib/actions.js
@@ -25,7 +25,6 @@ import getElasticsearchClient from './get_elasticsearch_client';
 import horsemanFactory from './horseman_factory';
 import url from 'url';
 
-var debug = true;
 var hlimit = config.sentinl.history ? config.sentinl.history : 10;
 
 export default function (server, actions, payload) {
@@ -48,9 +47,9 @@ export default function (server, actions, payload) {
   }
 
   /* Slack Settings */
-  var Slack = require('node-slack');
   var slack;
   if (config.settings.slack.active) {
+    var Slack = require('node-slack');
     slack = new Slack(config.settings.slack.hook);
   }
 
@@ -109,7 +108,7 @@ export default function (server, actions, payload) {
       }
     }).then(function (resp) {
     }, function (err, resp) {
-      console.trace(err, resp);
+      server.log(['status', 'info', 'Sentinl', 'ES'], err || resp);
     });
   };
 
