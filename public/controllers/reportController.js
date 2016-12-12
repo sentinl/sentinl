e9617daead839e9e034fedb76a92d9584a0a2bd8
Fabio Fortini <fabio.fortini@sorint.it>
2016-12-12 12:45:22 +0000

    issue-14: indentation fix

--- public/controllers/reportController.js
+++ public/controllers/reportController.js
@@ -17,7 +17,7 @@ uiModules
   timefilter.enabled = true;
 
   /* Update Time Filter */
-  var updateFilter = function () {
+  const updateFilter = function () {
     return $http.get('../api/sentinl/set/interval/' + JSON.stringify($scope.timeInterval).replace(/\//g, '%2F'));
   };
 
@@ -65,11 +65,9 @@ uiModules
     if (refreshValue !== $scope.currentRefresh && refreshValue !== 0) {
       // new refresh value
       if (_.isNumber(refreshValue) && !refreshPause) {
-        // console.log('TIMEFILTER REFRESH');
         $scope.newRefresh = refreshValue;
         // Reset Interval & Schedule Next
         $scope.refreshreports = $timeout(function () {
-          // console.log('Reloading data....');
           $route.reload();
         }, refreshValue);
         $scope.$watch('$destroy', $scope.refreshreports);
@@ -86,15 +84,15 @@ uiModules
 
   $scope.deleteReport = function ($index) {
     if ($window.confirm('Delete is Forever!\n Are you sure?')) {
-      return $http.get('../api/sentinl/delete/alarm/' + $scope.elasticReports[$index]._index
-      + '/' + $scope.elasticReports[$index]._type
-      + '/' + $scope.elasticReports[$index]._id)
+      return $http.get('../api/sentinl/delete/alarm/' +
+        $scope.elasticReports[$index]._index +
+        '/' + $scope.elasticReports[$index]._type +
+        '/' + $scope.elasticReports[$index]._id)
       .then(
         () => $timeout(function () {
           $scope.elasticReports.splice($index, 1);
           $scope.notify.warning('SENTINL Report log successfully deleted!');
-        }),
-        notify.error
+        }).catch(notify.error);
       );
     }
   };
@@ -103,11 +101,11 @@ uiModules
     notify.warning('SENTINL function not yet implemented!');
   };
 
-  var currentTime = moment($route.current.locals.currentTime);
+  const currentTime = moment($route.current.locals.currentTime);
   $scope.currentTime = currentTime.format('HH:mm:ss');
-  var utcTime = moment.utc($route.current.locals.currentTime);
+  const utcTime = moment.utc($route.current.locals.currentTime);
   $scope.utcTime = utcTime.format('HH:mm:ss');
-  var unsubscribe = $interval(function () {
+  const unsubscribe = $interval(function () {
     $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
     $scope.utcTime = utcTime.add(1, 'second').format('HH:mm:ss');
   }, 1000);
