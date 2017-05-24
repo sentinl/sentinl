233af2b78635274a83503299b4f324e2a439efd1
SergeyBondarenko <sergibondarenko@gmail.com>
2017-05-24 12:18:10 +0200

    _.contains is no more in lodash lib.

--- server/lib/handle_es_error.js
+++ server/lib/handle_es_error.js
@@ -31,7 +31,7 @@ module.exports = function handleESError(error) {
     error instanceof esErrors.NoConnections ||
     error instanceof esErrors.RequestTimeout) {
     return Boom.serverTimeout(error);
-  } else if (error instanceof esErrors.Conflict || _.contains(error.message, 'index_template_already_exists')) {
+  } else if (error instanceof esErrors.Conflict || _.includes(error.message, 'index_template_already_exists')) {
     return Boom.conflict(error);
   } else if (error instanceof esErrors[403]) {
     return Boom.forbidden(error);
