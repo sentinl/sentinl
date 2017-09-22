a40228ac377fdf178dee8f455e598a6201db2186
Felixls <sergioscarnatto@gmail.com>
2017-09-22 10:44:37 +0100

    Update sentinl to 5.5.2

--- public/services/saved_users/index.js
+++ public/services/saved_users/index.js
@@ -2,5 +2,5 @@ import './saved_user';
 import './saved_users';
 
 import savedUsersRegister from './saved_users_register';
-import savedObjectRegister from 'plugins/kibana/management/saved_object_registry';
-savedObjectRegister.register(savedUsersRegister);
+import { savedObjectManagementRegistry } from 'plugins/kibana/management/saved_object_registry';
+savedObjectManagementRegistry.register(savedUsersRegister);
