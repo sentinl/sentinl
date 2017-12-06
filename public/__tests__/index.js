9ee9333da42a0db6e8d2d0a187ae5072ba584335
sergibondarenko <sergibondarenko@gmail.com>
2017-12-06 11:55:08 +0100

    Add test for nextScheduleOccurence filter. Rename page controllers test files.

--- public/__tests__/index.js
+++ public/__tests__/index.js
@@ -1,11 +1,12 @@
 describe('Sentinl', function () {
-  require('../pages/__tests__/editorController');
-  require('../pages/__tests__/watchersController');
-  require('../pages/__tests__/reportsController');
-  require('../pages/__tests__/alarmsController');
+  require('../pages/__tests__/editor.controller');
+  require('../pages/__tests__/watchers.controller');
+  require('../pages/__tests__/reports.controller');
+  require('../pages/__tests__/alarms.controller');
   require('../services/__tests__/watcher');
   require('../services/__tests__/alarm');
   require('../services/__tests__/user');
   require('../services/__tests__/script');
   require('../services/__tests__/report');
+  require('../filters/__tests__/filters');
 });
