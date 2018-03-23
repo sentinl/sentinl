4960bc78190285c6ef71a779347105086ee8703f
Sergey Bondarenko <sergibondarenko@gmail.com>
2018-03-23 17:00:57 +0100

    Add executable path for puppeeter instance. Use promise delay. Do not throw error in doReport, log it.

--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -10,13 +10,6 @@ const isKibi = function (server) {
   return server.plugins.saved_objects_api ? true : false;
 };
 
-const delay = function (timeout) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-};
-
 module.exports = {
   isKibi,
-  delay,
 };
