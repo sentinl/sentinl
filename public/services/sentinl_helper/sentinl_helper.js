0dd4c145f9a0338719f7161025dcd2eb45f634db
Sergey Bondarenko <sergibondarenko@gmail.com>
2018-05-03 17:30:39 +0200

    Add a test for sentinlHelper.

---
+++ public/services/sentinl_helper/sentinl_helper.js
@@ -0,0 +1,11 @@
+import { isObject, stripObjectPropertiesByNameRegex } from '../../lib/sentinl_helper';
+
+class SentinlHelper {
+  constructor() {}
+
+  stripObjectPropertiesByNameRegex(obj, nameRegexp) {
+    stripObjectPropertiesByNameRegex(obj, nameRegexp);
+  }
+}
+
+export default SentinlHelper;
