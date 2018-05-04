7a0fca82a8d67fd7e832f34b975b10a918efaede
Sergey Bondarenko <sergibondarenko@gmail.com>
2018-05-04 17:56:28 +0200

    Set default Chromium path for report execution.
    Use klaw lib to parse puppeteer dirs recursively to find chrome file.

--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -1,4 +1,6 @@
 import Promise from 'bluebird';
+import path from 'path';
+import klawSync from 'klaw-sync';
 
 /**
 * Check if Kibi
@@ -10,6 +12,22 @@ const isKibi = function (server) {
   return server.plugins.saved_objects_api ? true : false;
 };
 
+/**
+* Get full sys path by file name
+*
+* @param {string} fileName
+* @param {string} rootPath where to look for the fileName
+* @return {string} full sys path to fileName
+*/
+const getFullPathByFileName = function (fileName, rootPath) {
+  const paths = klawSync(rootPath);
+  let found = paths.filter((e) => path.basename(e.path) === fileName);
+  if (Array.isArray(found) && !!found.length) {
+    return found[0].path;
+  }
+};
+
 module.exports = {
   isKibi,
+  getFullPathByFileName,
 };
