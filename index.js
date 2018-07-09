837a7cb3ab844476b5a26433ec0751d6ce21e96f
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-07-09 18:22:23 +0200

    Set Siren defaults.

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ export default function (kibana) {
           }).default(),
           report: Joi.object({
             active: Joi.boolean().default(true),
-            engine: Joi.string().default('horseman'), // options: puppeteer, horseman
+            engine: Joi.string().default('puppeteer'), // options: puppeteer, horseman
             phantomjs_path: Joi.string().allow('').default(phantomjsDefaultPath || ''),
             chrome_path: Joi.string().allow('').default(chromeDefaultPath || ''),
             executable_path: Joi.any().forbidden().error(new Error(
--- scripts/createReleases.sh
+++ scripts/createReleases.sh
@@ -5,21 +5,11 @@
 #     ./createReleases.sh ~/Downloads/6/
 
 releases=(
-"6.0.0"
-"6.0.1"
-"6.1.0"
-"6.1.1"
-"6.1.2"
-"6.1.3"
-"6.2.0"
-"6.2.1"
-"6.2.2"
-"6.2.3"
-"6.2.4"
+"5.6.10"
 )
 
 for i in "${releases[@]}"
 do
 	gulp package --version=$i && mv ../target/gulp/sentinl* $1
-	# gulp package_nochrome --version=$i && mv ../target/gulp/sentinl* $1
+	#gulp package_nochrome --version=$i && mv ../target/gulp/sentinl* $1
 done
