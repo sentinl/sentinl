5bc4981fb7c81f55c6c1a1b455bfb24b22ec91f2
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-07-31 10:55:58 +0200

    Add API routes to translate SQL query text to ES DSL query.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -8,6 +8,7 @@ import Crypto from '../lib/crypto';
 import WatcherHandler from '../lib/watcher_handler';
 import Log from '../lib/log';
 import { isKibi } from '../lib/helpers';
+import { convert as convertSQLtoDSL } from 'elasql';
 
 const delay = function (ms) {
   return new Promise(function (resolve) {
@@ -496,6 +497,71 @@ export default function routes(server) {
         return reply({
           username: get(user, '_source.username') || get(user, `_source[${config.es.user_type}].username`),
         });
+      } catch (err) {
+        return reply(handleESError(err));
+      }
+    }
+  });
+
+  server.route({
+    path: '/api/sentinl/sql/translate',
+    method: 'POST',
+    config: {
+      validate: {
+        payload: {
+          sql_query: Joi.string(),
+        },
+      },
+    },
+    handler: async function (req, reply) {
+      try {
+        return reply({
+          dsl_query: convertSQLtoDSL(req.payload.sql_query), // DSL query
+        });
+      } catch (err) {
+        return reply(handleESError(err));
+      }
+    }
+  });
+
+  server.route({
+    path: '/api/sentinl/sql/execute/{index}',
+    method: 'POST',
+    config: {
+      validate: {
+        params: {
+          index: Joi.string(),
+        },
+        payload: {
+          sql_query: Joi.string(),
+        },
+      },
+    },
+    handler: async function (req, reply) {
+      const sqlQuery = req.payload.sql_query;
+      const index = req.params.index;
+
+      try {
+        const apiSqlTranslate = await server.inject({
+          method: 'POST',
+          url: '/api/sentinl/sql/translate',
+          headers: {
+            'kbn-xsrf': 'reporting',
+          },
+          payload: {
+            sql_query: sqlQuery,
+          },
+        });
+
+        const body = apiSqlTranslate.result.dsl_query;
+        body.size = config.es.results;
+
+        const resp = await client.search({
+          index,
+          body,
+          type: config.es.default_type,
+        });
+        return reply(resp);
       } catch (err) {
         return reply(handleESError(err));
       }
