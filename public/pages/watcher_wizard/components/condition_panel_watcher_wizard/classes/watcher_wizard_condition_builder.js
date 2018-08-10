ac6ae5a069ad43b844a5d8d9ade6da846ec603be
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-08-10 10:28:35 +0200

    Fix watcher condition scripts. (#695)

--- public/pages/watcher_wizard/components/condition_panel_watcher_wizard/classes/watcher_wizard_condition_builder.js
+++ public/pages/watcher_wizard/components/condition_panel_watcher_wizard/classes/watcher_wizard_condition_builder.js
@@ -24,61 +24,62 @@ class WatcherWizardConditionBuilder {
   /*
   * @return {string}
     // if (over.type === 'all docs')
-    "payload.hits.total > 0"
+    "payload.aggregations.dateAgg.buckets.some(b => b.doc_count>1)"
     // else
-    "for(var r=payload.aggregations.bucketAgg.buckets,t=0;t<r.length;t++)if(r[t].doc_count>0)return!0;return!1"
+    "payload.aggregations.bucketAgg.buckets.some(b => b.dateAgg.buckets.some(b => b.doc_count>1))"
   */
-  count({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
+  count({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}, filterMethod = 'some'} = {}) {
     const compToThis = this._mathSmbl(threshold.direction) + threshold.n;
 
     if (over.type === 'all docs') {
-      return `payload.hits.total ${compToThis}`;
+      return `payload.aggregations.dateAgg.buckets.${filterMethod}(b => b.doc_count${compToThis})`;
     }
-    return `payload.aggregations.bucketAgg.buckets.some(e => e.doc_count${compToThis})`;
+    return `payload.aggregations.bucketAgg.buckets.${filterMethod}(b => b.dateAgg.buckets.${filterMethod}(b => b.doc_count${compToThis}))`;
   }
 
   /*
   * @return {string}
   */
-  average({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
-    return this._metricAggScript({over, threshold});
+  average({over, threshold, filterMethod}) {
+    return this._metricAgg({over, threshold, filterMethod});
   }
 
   /*
   * @return {string}
   */
-  sum({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
-    return this._metricAggScript({over, threshold});
+  sum({over, threshold, filterMethod}) {
+    return this._metricAgg({over, threshold, filterMethod});
   }
 
   /*
   * @return {string}
   */
-  min({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
-    return this._metricAggScript({over, threshold});
+  min({over, threshold, filterMethod}) {
+    return this._metricAgg({over, threshold, filterMethod});
   }
 
   /*
   * @return {string}
   */
-  max({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
-    return this._metricAggScript({over, threshold});
+  max({over, threshold, filterMethod}) {
+    return this._metricAgg({over, threshold, filterMethod});
   }
 
   /*
   * @return {string}
     // if (over.type === 'all docs')
-    "payload.aggregations.metricAgg.value >0"
+    "payload.aggregations.dateAgg.buckets.some(b => b.metricAgg.value>1)"
     // else
-    for(var r=payload.aggregations.bucketAgg.buckets,a=0;a<r.length;a++)if(r[a].metricAgg.value>0)return!0;return!1
+    "payload.aggregations.bucketAgg.buckets.some(b => b.dateAgg.buckets.some(b => b.metricAgg.value>1))"
   */
-  _metricAggScript({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}} = {}) {
+  _metricAgg({over = {type: 'all docs'}, threshold = {n: 0, direction: 'above'}, filterMethod = 'some'} = {}) {
     const compToThis = this._mathSmbl(threshold.direction) + threshold.n;
 
     if (over.type === 'all docs') {
-      return `payload.aggregations.metricAgg.value ${compToThis}`;
+      return `payload.aggregations.dateAgg.buckets.${filterMethod}(b => b.metricAgg.value${compToThis})`;
     }
-    return `payload.aggregations.bucketAgg.buckets.some(e => e.metricAgg.value${compToThis})`;
+    return `payload.aggregations.bucketAgg.buckets.${filterMethod}` +
+      `(b => b.dateAgg.buckets.${filterMethod}(b => b.metricAgg.value${compToThis}))`;
   }
 }
 
