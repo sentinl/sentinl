1ba48193d43115e48a03edcdc996e95483327ab4
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-10-15 16:56:15 +0200

    Refactor wizard condition components using EUI react components.
    Update agg field on type change.
    
    Fix 'month' unit.
    
    Fix typo, pass correctly the 1st numAggField.
    
    Delete unused lib imports.
    
    Create a UiCodeEditor component and apply it to several places. To-do: substitude all ace editors by this component.
    It is done to stop this error in the browser dev console https://github.com/ajaxorg/ace/issues/1460
    
    Put directive arguments inside double quotes.
    
    Remove unused lib imports.
    
    Upgrade elastic/eui version.
    
    Improve UiCodeEditor code style.
    
    Lift state up for the wizard condition panel expression component.
    
    Improve code style.

--- public/pages/watcher_wizard/components/condition_panel_watcher_wizard/components/dd_watcher_agg_over/index.js
+++
@@ -1,5 +0,0 @@
-import { uiModules } from 'ui/modules';
-import ddWatcherAggOver from './dd_watcher_agg_over';
-
-const module = uiModules.get('apps/sentinl');
-module.directive('ddWatcherAggOver', ddWatcherAggOver);
