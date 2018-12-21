bf3259b555075770bf1c64b7f7f917856906e50a
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-12-21 12:40:00 +0100

    Add all puppeteer options for report screnshot and pdf.

--- server/lib/actions/report/index.js
+++ server/lib/actions/report/index.js
@@ -40,30 +40,6 @@ export default async function reportAction({
     const browserPath = server.plugins.sentinl.chrome_path;
     const { subject, text } = renderMustacheEmailSubjectAndText(actionName, action.report.subject, action.report.body, esPayload);
 
-    let authSelectorUsername = action.report.auth.selector_username || config.settings.report.auth.username;
-    let authSelectorPassword = action.report.auth.selector_password || config.settings.report.auth.username;
-    let authSelectorLoginBtn = action.report.auth.selector_login_btn;
-    const collapseNavbarSelector = get(action.report, 'selectors.collapse_navbar_selector') ||
-      config.settings.report.css_selectors.collapse_navbar_selector;
-
-    if (action.report.auth.active) {
-      if (!config.settings.report.auth.modes.includes(action.report.auth.mode)) {
-        throw new Error('wrong auth mode, available modes: ' + config.settings.report.auth.modes.toString());
-      }
-
-      if (action.report.auth.mode === 'xpack') {
-        authSelectorUsername = authSelectorUsername || config.settings.report.auth.css_selectors.xpack.username;
-        authSelectorPassword = authSelectorPassword || config.settings.report.auth.css_selectors.xpack.password;
-        authSelectorLoginBtn = authSelectorLoginBtn || config.settings.report.auth.css_selectors.xpack.login_btn;
-      }
-
-      if (action.report.auth.mode === 'searchguard') {
-        authSelectorUsername = authSelectorUsername || config.settings.report.auth.css_selectors.searchguard.username;
-        authSelectorPassword = authSelectorPassword || config.settings.report.auth.css_selectors.searchguard.password;
-        authSelectorLoginBtn = authSelectorLoginBtn || config.settings.report.auth.css_selectors.searchguard.login_btn;
-      }
-    }
-
     log.info(`${watcherTitle}, executing report by "${config.settings.report.engine}"`);
     log.info(`${watcherTitle}, browser: "${browserPath}"`);
 
@@ -77,32 +53,7 @@ export default async function reportAction({
 
     const fileName = createReportFileName(action.report.snapshot.name, action.report.snapshot.type);
     let filePath = path.join(os.tmpdir(), fileName);
-
-    filePath = await puppeteerReport({
-      filePath,
-      reportUrl: action.report.snapshot.url,
-      fileType: action.report.snapshot.type,
-      fileUrl: action.report.snapshot.url,
-      timeout: action.report.snapshot.params.delay,
-      pdfFormat: action.report.snapshot.pdf_format,
-      pdfLandscape: action.report.snapshot.pdf_landscape,
-      viewPortWidth: action.report.snapshot.res.split('x')[0],
-      viewPortHeight: action.report.snapshot.res.split('x')[1],
-      authMode: action.report.auth.mode,
-      authActive: action.report.auth.active,
-      authUsername: action.report.auth.username,
-      authPassword: action.report.auth.password,
-      authSelectorUsername,
-      authSelectorPassword,
-      authSelectorLoginBtn,
-      ignoreHTTPSErrors: config.settings.report.ignore_https_errors,
-      chromeHeadless: config.settings.report.puppeteer.chrome_headless,
-      chromeDevtools: config.settings.report.puppeteer.chrome_devtools,
-      chromeArgs: config.settings.report.puppeteer.chrome_args,
-      collapseNavbarSelector,
-      browserPath,
-      server,
-    });
+    filePath = await puppeteerReport({ action, filePath, browserPath, server });
 
     log.info(`${watcherTitle}, '${config.settings.report.engine}' report results: '${filePath}'`);
 
--- server/lib/actions/report/puppeteer.js
+++ server/lib/actions/report/puppeteer.js
@@ -2,49 +2,51 @@ import puppeteer from 'puppeteer';
 import { ActionError } from '../../errors';
 import getConfiguration from '../../get_configuration';
 import Log from '../../log';
+import { isEmpty, get } from 'lodash';
 
 export default async function puppeteerReport({
   browserPath,
   filePath,
-  fileType,
-  reportUrl,
-  timeout,
-  viewPortWidth,
-  viewPortHeight,
-  pdfFormat,
-  pdfLandscape,
-  authMode,
-  authActive,
-  authUsername,
-  authPassword,
-  authSelectorUsername,
-  authSelectorPassword,
-  authSelectorLoginBtn,
-  ignoreHTTPSErrors,
-  chromeArgs = ['--no-sandbox', '--disable-setuid-sandbox'],
-  chromeHeadless = true,
-  chromeDevtools = false,
-  collapseNavbarSelector,
+  action,
   server
 }) {
   let browser;
   let page;
   const config = getConfiguration(server);
   const log = new Log(config.app_name, server, 'report');
+  const timeout = +action.report.snapshot.params.delay;
+  const viewPortWidth = +action.report.snapshot.res.split('x')[0];
+  const viewPortHeight = +action.report.snapshot.res.split('x')[1];
+  const ignoreHTTPSErrors = config.settings.report.ignore_https_errors;
+  const chromeHeadless = config.settings.report.puppeteer.chrome_headless;
+  const chromeDevtools = config.settings.report.puppeteer.chrome_devtools;
+  const chromeArgs = config.settings.report.puppeteer.chrome_args;
 
-  try {
-    try {
-      timeout = +timeout;
-      viewPortWidth = +viewPortWidth;
-      viewPortHeight = +viewPortHeight;
-      authActive = String(authActive) === 'true';
-      ignoreHTTPSErrors = String(ignoreHTTPSErrors) === 'true';
-      chromeHeadless = String(chromeHeadless) === 'true';
-      chromeDevtools = String(chromeDevtools) === 'true';
-    } catch (err) {
-      throw new ActionError('sanitize args', err);
+  let authSelectorUsername = action.report.auth.selector_username || config.settings.report.auth.username;
+  let authSelectorPassword = action.report.auth.selector_password || config.settings.report.auth.username;
+  let authSelectorLoginBtn = action.report.auth.selector_login_btn;
+  const collapseNavbarSelector = get(action.report, 'selectors.collapse_navbar_selector') ||
+    config.settings.report.css_selectors.collapse_navbar_selector;
+
+  if (action.report.auth.active) {
+    if (!config.settings.report.auth.modes.includes(action.report.auth.mode)) {
+      throw new Error('wrong auth mode, available modes: ' + config.settings.report.auth.modes.toString());
     }
 
+    if (action.report.auth.mode === 'xpack') {
+      authSelectorUsername = authSelectorUsername || config.settings.report.auth.css_selectors.xpack.username;
+      authSelectorPassword = authSelectorPassword || config.settings.report.auth.css_selectors.xpack.password;
+      authSelectorLoginBtn = authSelectorLoginBtn || config.settings.report.auth.css_selectors.xpack.login_btn;
+    }
+
+    if (action.report.auth.mode === 'searchguard') {
+      authSelectorUsername = authSelectorUsername || config.settings.report.auth.css_selectors.searchguard.username;
+      authSelectorPassword = authSelectorPassword || config.settings.report.auth.css_selectors.searchguard.password;
+      authSelectorLoginBtn = authSelectorLoginBtn || config.settings.report.auth.css_selectors.searchguard.login_btn;
+    }
+  }
+
+  try {
     browser = await puppeteer.launch({
       headless: chromeHeadless,
       devtools: chromeDevtools,
@@ -60,27 +62,27 @@ export default async function puppeteerReport({
       height: viewPortHeight,
     });
 
-    if (authActive && authMode === 'basic') { // for test: http://httpbin.org/basic-auth/user/passwd (username: user, password: passwd)
+    if (action.report.auth.active && action.report.auth.mode === 'basic') { // for test: http://httpbin.org/basic-auth/user/passwd (username: user, password: passwd)
       await page.setExtraHTTPHeaders({
-        Authorization: `Basic ${new Buffer(`${authUsername}:${authPassword}`).toString('base64')}`,
+        Authorization: `Basic ${new Buffer(`${action.report.auth.username}:${action.report.auth.password}`).toString('base64')}`,
       });
     }
 
-    await page.goto(reportUrl, { timeout });
+    await page.goto(action.report.snapshot.url, { timeout });
     await page.waitFor(timeout);
 
     try {
-      if (authActive && authMode !== 'basic') { // for test: http://testing-ground.scraping.pro/login (username: admin, password: 12345)
+      if (action.report.auth.active && action.report.auth.mode !== 'basic') { // for test: http://testing-ground.scraping.pro/login (username: admin, password: 12345)
         await page.waitForSelector(authSelectorUsername, { timeout, visible: true });
-        await page.type(authSelectorUsername, authUsername);
+        await page.type(authSelectorUsername, action.report.auth.username);
         await page.waitForSelector(authSelectorPassword, { timeout, visible: true });
-        await page.type(authSelectorPassword, authPassword);
+        await page.type(authSelectorPassword, action.report.auth.password);
         await page.waitForSelector(authSelectorLoginBtn, { timeout, visible: true });
         await page.click(authSelectorLoginBtn);
         await page.waitFor(timeout);
       }
     } catch (err) {
-      throw new ActionError('login form', err);
+      log.error('login form', err);
     }
 
     try {
@@ -93,17 +95,48 @@ export default async function puppeteerReport({
       log.error('collapse navbar', err);
     }
 
-    if (fileType === 'pdf') {
-      await page.pdf({
+    if (action.report.snapshot.type === 'pdf') {
+      const options = {
         path: filePath,
-        format: pdfFormat,
-        landscape: pdfLandscape,
+        format: action.report.snapshot.pdf_format,
+        landscape: action.report.snapshot.pdf_landscape,
+        scale: action.report.snapshot.pdf_scale,
+        displayHeaderFooter: action.report.snapshot.pdf_display_header_footer,
+        headerTemplate: action.report.snapshot.pdf_header_template,
+        footerTemplate: action.report.snapshot.pdf_footer_template,
+        printBackground: action.report.snapshot.pdf_print_background,
+        pageRanges: action.report.snapshot.pdf_page_ranges,
+        preferCSSPageSize: action.report.snapshot.pdf_prefer_css_page_size,
+      };
+
+      if (!isEmpty(action.report.snapshot.pdf_margin)) {
+        options.margin = action.report.snapshot.pdf_margin;
+      }
+
+      ['pdf_width', 'pdf_height'].forEach(key => {
+        if (action.report.snapshot[key]) {
+          options[key.split('_')[1]] = action.report.snapshot[key];
+        }
       });
+
+      await page.pdf(options);
     } else {
-      await page.screenshot({
+      const options = {
         path: filePath,
-        type: fileType,
-      });
+        type: action.report.snapshot.screenshot_type,
+        omitBackground: action.report.snapshot.screenshot_omit_background,
+        fullPage: action.report.snapshot.screenshot_full_page
+      };
+
+      if (!isEmpty(action.report.snapshot.screenshot_clip)) {
+        options.clip = action.report.snapshot.screenshot_clip;
+      }
+
+      if (action.report.snapshot.screenshot_type !== 'png') {
+        options.quality = action.report.snapshot.screenshot_quality;
+      }
+
+      await page.screenshot(options);
     }
 
     await browser.close();
